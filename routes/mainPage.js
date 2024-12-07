a1a7b882d7dee7abf888fb252d7f4b3440c17f84
Tahm24 <156429364+Tahm24@users.noreply.github.com>
2024-12-07 22:28:25 +0000

    qq

--- routes/mainPage.js
+++ routes/mainPage.js
@@ -27,7 +27,7 @@ const redirectLogin = (req, res, next) => {
 
 ///////////////////////Home Page///////////////////////
 //Home Page loading recent movies
-router.get("/", redirectLogin, (req, res) => {
+router.get("/", (req, res) => {
     //page number using queries and parse into url to load page numbers, default value set to 1 page
     const page = req.query.page || 1; 
     const url = `${TMDB_BASE_URL}/movie/now_playing?api_key=${TMDB_API_KEY}&language=en-US&page=${page}`;
